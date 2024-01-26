d4591ffd7c63450b144707782e08c02b80af1237
mateussfortes <mateussilveirafortes@gmail.com>
2024-01-26 17:36:54 -0300

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export default function HomeScreen() {
+  const router = useRouter();
+  const [values, setValues] = React.useState({
+    usuario: "omariosouto",
+    senha: "safepassword",
+  });
+
+  function handleChange(event) {
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
+    setValues((currentValues) => {
+      return {
+        ...currentValues,
+        [fieldName]: fieldValue,
+      };
+    });
+  }
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.push("/auth-page-ssr");
+        }}
+      >
+        <input
+          placeholder="Usuário"
+          name="usuario"
+          defaultValue="omariosouto"
+          value={values.usuario}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="Senha"
+          name="senha"
+          type="password"
+          defaultValue="safepassword"
+          value={values.senha}
+          onChange={handleChange}
+        />
+        <div>
+          <button>Entrar</button>
+        </div>
+        <pre>{JSON.stringify(values, null, 2)}</pre>
+      </form>
+    </div>
+  );
+}
